2fab1d23846d6f337ed2ab4213e77a5bb571f49c
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-11-02 23:40:47 +0000

    agregue el get

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+const [task, setTask] = useState({ "label": "", "done": false })
+const [taskList, setTaskList] = useState([])
+const cambio = (e) => {
+	setTask({ ...task, "label": e.target.value }
+	)
+}
+const getTask = async () => {
+	try {
+		const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/pametr88");
+		if(response.ok){
+			let data = await response.json();
+			setTaskList(data)
+		}
+	}
+	catch (error) {
+		console.log("Error:", error);
+	}
+}
+useEffect(() => {
+	getTask();
+}, [])
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">TO DO LIST</h1>
+			<input type="text" placeholder="agrega la tarea"/>
+			<ul>
+				{taskList.map((item,index)=>{
+					return (
+						<li key={index}>{item.label}</li>
+					)
+				})}				
+			</ul>
 		</div>
 	);
 };
