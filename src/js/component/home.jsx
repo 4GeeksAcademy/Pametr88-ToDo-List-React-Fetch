e1e097a9f0d141970e86c5e3c7dbf14c885a0c86
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-11-04 00:09:27 +0000

    agregue estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,42 +1,97 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";//include images into your bundle//create your first component
 const Home = () => {
-//include images into your bundle
-const [task, setTask] = useState({ "label": "", "done": false })
-const [taskList, setTaskList] = useState([])
-// const cambio = (e) => {
-// 	setTask({ ...task, "label": e.target.value }
-// 	)
-// }
-const getTask = async () => {
-	try {
-		const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/pametr88");
-		if(response.ok){
-			let data = await response.json();
-			setTaskList(data)
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	useEffect(() => {
+		createUser()
+		takeList()
+	}, [])
+	useEffect(() => { sendList() }, [todos]);
+	const createUser = async () => {
+		try {
+			const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/', {
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			if (!response.ok) {
+				throw Error(response.statusText);
+			}
+			const transform = await response.json()
+			console.log(transform.msg)
+			return transform
+		} catch (e) { }
+	}
+	const takeList = async () => {
+		try {
+			const previewResponse = await fetch(
+				'https://playground.4geeks.com/apis/fake/todos/user/'
+			)
+			if (!previewResponse.ok) {
+				throw Error(previewResponse.statusText);
+			} const transform = await previewResponse.json()
+			setTodos(transform)
+		} catch (e) {
+			console.log("error", e)
 		}
 	}
-	catch (error) {
-		console.log("Error:", error);
+	const sendList = async () => {
+		try {
+			const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/', {
+				method: "PUT",
+				body: JSON.stringify(todos),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			if (!response.ok) {
+				throw Error(response.statusText);
+			} const transform = await response.json()
+			console.log(transform.msg)
+			return transform
+		} catch (e) { }
 	}
-}
-useEffect(() => {
-	getTask();
-}, [])
-//create your first component
-
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">TO DO LIST</h1>
-			<input type="text" placeholder="agrega la tarea"/>
-			<ul>
-				{taskList.map((item,index)=>{
-					return (
-						<li key={index}>{item.label}</li>
-					)
-				})}				
-			</ul>
-		</div>
+	const pressEnter = async (e) => {
+		try {
+			if (e.key === "Enter" && inputValue !== "") {
+				let obj = {
+					label: inputValue,
+					done: false
+				}
+				setTodos([...todos, obj]);
+				setInputValue(""); const final = await sendList()
+				console.log("informacion enviada", final)
+			}
+		}
+		catch (e) {
+			console.log("Hubo un error", e)
+		}
+	}
+	return (<div className="Container">
+		<h1 className="title">Todos</h1>
+		<ul className="list-group list-group-flush">
+			<input
+				type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyDown={(e) => pressEnter(e)}
+				placeholder="What do you need to do?" />
+			{todos.map((item, index) => (<li className="list-group-item li-c" key={index}>
+				{item.label}
+				<button onClick={() => {
+					const updatedTodos = todos.filter(
+						(_, currentIndex) => index != currentIndex
+					); setTodos(updatedTodos);
+				}}>
+					<i className="fa-solid fa-trash-can"></i>
+				</button>
+			</li>
+			))}
+		</ul>
+		<div className="items">{todos.length} items left</div>
+	</div>
 	);
 };
-
 export default Home;
